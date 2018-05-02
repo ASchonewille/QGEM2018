edafab4f7b3ea8dff9f245ccd2df758f0604e63a
ericgrewalgithub <38923140+ericgrewalgithub@users.noreply.github.com>
2018-05-02 15:40:30 -0400

    updated Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -8,3 +8,37 @@ Start Date: May 2, 2018
 This program was made to compute the calculation of concentration of cortisol
 """
 
+"""
+
+GLOBAL VARIABLES
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+global cortisolBindingRate
+global luciferaseUnitLight
+global samplingPeriod
+global discardPeriod
+
+
+def plotGraph(A):
+
+    B = np.array(A)
+    plt.imshow(H)
+    plt.show()
+    return
+
+
+
+
+
+
+
+def main():
+    A =([[1,2,3],[1,2,3]])  
+
+main()
+
