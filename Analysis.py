a677b5b5911cdb55cac8827ce018d125f9db1886
ericgrewalgithub <38923140+ericgrewalgithub@users.noreply.github.com>
2018-05-02 17:02:24 -0400

    updated Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -8,11 +8,7 @@ Start Date: May 2, 2018
 This program was made to compute the calculation of concentration of cortisol
 """
 
-"""
 
-GLOBAL VARIABLES
-
-"""
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -22,6 +18,7 @@ global cortisolBindingRate
 global luciferaseUnitLight
 global samplingPeriod
 global discardPeriod
+global targetFunction
 
 
 def plotGraph(x,y):
@@ -32,16 +29,30 @@ def plotGraph(x,y):
     plt.show()
     return
 
+def determineFunction(x,y):
 
+    function = np.polyfit(x,y,1,rcond=None, full=False)
+    a = function[0]
+    b = function[1]
+    return a,b
 
+def lambdaFunction(a,b):
 
-
-
-
+    global targetFunction    
+    targetFunction = lambda x : a*x + b
+    return 
+    
+   
 def main():
+    global targetFunction
     x = [1,2,3]
-    y = [1,2,3]
+    y = [2,3,4]
     plotGraph(x,y)
+    a,b = determineFunction(x,y)
+    lambdaFunction(a,b)
+    print (targetFunction)
+    print (a,b)
 
+    
 main()
 
