bd55abacaf9363823c6ecc6ddf57d87e3ebc1a75
ericgrewalgithub <38923140+ericgrewalgithub@users.noreply.github.com>
2018-05-03 09:49:15 -0400

    updated Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -12,7 +12,7 @@ This program was made to compute the calculation of concentration of cortisol
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+import scipy.misc as sci
 
 global cortisolBindingRate
 global luciferaseUnitLight
@@ -42,7 +42,19 @@ def lambdaFunction(a,b):
     targetFunction = lambda x : a*x + b
     return 
     
-   
+
+def cutData(x,y):
+    lengthX = len(x)
+    lengthY = len(y)
+    xCut = x[1:lengthX]
+    yCut = y[1:lengthY]
+    return xCut, yCut
+
+
+
+def takeDerivative(targetFunction, x0):
+    derivative = sci.derivative(targetFunction, x0)
+    return
 def main():
     global targetFunction
     x = [1,2,3]
@@ -50,9 +62,14 @@ def main():
     plotGraph(x,y)
     a,b = determineFunction(x,y)
     lambdaFunction(a,b)
-    print (targetFunction)
-    print (a,b)
+    xCut,yCut = cutData(x,y)
+    x0 = 1
+    derivative = takeDerivative(targetFunction, x0)
 
-    
+    print(targetFunction)
+    print (a,b)
+    print (xCut, yCut)
+    print (derivative)
+        
 main()
 
