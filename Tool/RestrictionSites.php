b7cb9e92b084989638aa8ad84da61220f0f5f1ed
ASchonewille <abigael.schonewille@gmail.com>
2018-05-09 15:41:32 -0400

    Organization and beginning of the Software tool

---
+++ Tool/RestrictionSites.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Standard Meta -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+    <!-- Site Properties -->
+    <title>SAM Movies - Home</title>
+    <link rel="stylesheet" type="text/css" href=   "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.0/semantic.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.0/semantic.min.js"></script>
+
+    <style>
+        .wrapper {
+            margin-top: 20px;
+        }
+    </style>
+</head>
+
+<body>
+<div class="wrapper">
+<div class="ui container">
+    <div class='ui pointing menu'>
+		<div class= "ui tiny image">
+			<img src="QGEMLogo.jpg">
+        </div>
+        <a class='item' href='./home.php'>
+            Home
+        </a>
+        <a class='active item blue' href='./RestrictionSites.php'>
+            Restriction Sites
+        </a>
+
+    </div>
+	
+  <div class="ui segment">
+    <h1>Restriction Site Elimination</h1>
+    <p>How to use this tool</p>
+  </div>
+  
+  <div class="ui segment">  
+	<form class="ui form">
+		<div class="field">
+			<label>Target Sequence</label>
+			<textarea name="sequence"></textarea> 
+		</div>
+		
+		<div class="ui right labeled input">
+			<input type="text" name="inputFile" placeholder="Choose a file">
+			<label for="sequenceFile" class="ui button">
+				Browse Files
+			</label>
+			<input type="file" name="sequenceFile" style="display: none">
+		</div>
+	</form>  
+  </div>
+  
+</div>
+</div>  
+
+</body>
+
+</html>
---
+++ Tool/home.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Standard Meta -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+    <!-- Site Properties -->
+    <title>SAM Movies - Home</title>
+    <link rel="stylesheet" type="text/css" href=   "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.0/semantic.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.0/semantic.min.js"></script>
+
+    <style>
+        .wrapper {
+            margin-top: 20px;
+        }
+    </style>
+</head>
+
+<body>
+<div class="wrapper">
+<div class="ui container">
+    <div class='ui pointing menu'>
+		<div class= "ui tiny image">
+			<img src="QGEMLogo.jpg">
+        </div>
+        <a class='active item blue' href='./home.php'>
+            Home
+        </a>
+        <a class='item' href='./RestrictionSites.php'>
+            Restriction Sites
+        </a>
+
+      </div>
+  <div class="ui segment">
+    <h1>Intro</h1>
+    <p>Description of the project, why we are doing this, what it accomplishes what is it for</p>
+  </div>
+</div>
+</div>
+</body>
+
+</html>
