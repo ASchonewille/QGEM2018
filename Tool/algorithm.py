781fd550caeccd7347f4e32f4bcd9796167af8fb
ASchonewille <abigael.schonewille@gmail.com>
2018-05-22 09:09:31 -0400

    Modification of restriction enzyme tree

--- Tool/algorithm.py
+++ Tool/algorithm.py
@@ -8,32 +8,79 @@ restrictionEnzymes = { "AatII" : "GACGTC",
                        "AflII" : "CTTAAG" }
 
 class node:
-    def __init__(self, value, childList):
+    def __init__(self, value):
         self.value = value
-        self.children = setChildren(childList)
-
-    def setChildren(childList):
-        for i in childList:
-            self.addChild(i)
+        self.children = {}
         return
 
-    def addChild(self, childValue):
-        placed = False
-        i = 0
-        while i < range(self.children) || !placed:
-            if childValue < self.children[i]:
-                self.children.insert(i, node(childValue, [])
-                placed = True
-            i += 1
-        return                 
+    def __eq__(self, other):
+        if self.value == other.value:
+            return True
+        else:
+            return False
+
+    def __repr__(self):
+        return self.value
+
+    def __hash__(self):
+        return hash(self.value)
+
+    def __iter__(self):
+        return self
+
+    def printChildren(self):
+        childString = ""
+        for i in self.children:
+            childString += self.children[i] + '/'
+        print(childString)
+        return
+    
+    def addChild(self, value):
+        if (value not in self.children):
+            self.children[value] = node(value)
+        else:
+            print("Already in child set")
+        return
+
+def addRestrictionSite(Tree, site):
+    if (not site):
+        Tree.addChild("E")
+    else:
+        if (site[0] == "A" or site[0] == "D" or site[0] == "H" or site[0] == "M" or site[0] == "N" or site[0] == "R" or site[0] == "V" or site[0] == "W"):
+
+            if ("A" not in Tree.children):
+                Tree.addChild("A")                        
+            Tree.children = addRestrictionSite(Tree.children["A"], site[1:])
+            
+        if (site[0] == "C" or site[0] == "B" or site[0] == "H" or site[0] == "M" or site[0] == "N" or site[0] == "S" or site[0] == "V" or site[0] == "Y"):
+            if ("C" not in Tree.children):
+                Tree.addChild("C")                        
+            Tree.children = addRestrictionSite(Tree.children["C"], site[1:])
+            
+        if (site[0] == "G" or site[0] == "B" or site[0] == "D" or site[0] == "K" or site[0] == "N" or site[0] == "R" or site[0] == "S" or site[0] == "V"):
+            if ("G" not in Tree.children):
+                Tree.addChild("G")                        
+            Tree.children = addRestrictionSite(Tree.children["G"], site[1:])
+             
+        if (site[0] == "T" or site[0] == "B" or site[0] == "D" or site[0] == "H" or site[0] == "K" or site[0] == "N" or site[0] == "W" or site[0] == "Y"):
+            if ("T" not in Tree.children):
+                Tree.addChild("T")                        
+            Tree.children = addRestrictionSite(Tree.children["T"], site[1:])
+   
+    return Tree 
 
 def BuildEnzymeTree(enzymes):
-    Tree = node("Root", [])
-    for i in enzymes: 
+    Tree = node("Root")
+    print(Tree.value)
+    print(Tree.children)
+    Tree = addRestrictionSite(Tree, "ATCG")
+    Tree.printChildren()
+    
 
-def Main():
+def Main():    
     enzymes = ["AatII", "AciI", "AfeI"]
     Tree = BuildEnzymeTree(enzymes)
+    
     
 
 Main()
